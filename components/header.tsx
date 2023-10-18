d8873aaeba038ecb911de3c58ee0f96f6d0a3c06
Alan Mendelevich <ailon@ailon.org>
2023-10-18 10:09:36 +0300

    add marker.js and github links to header

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import Link from "next/link";
-import { QuestionMarkIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import { GitHubLogoIcon, QuestionMarkIcon } from "@radix-ui/react-icons";
 
 type Props = {};
 
@@ -17,6 +18,23 @@ const Header = (props: Props) => {
           Diagrams
         </Link>
         <div className="flex flex-1 justify-end">
+          <a href="https://markerjs.com/products/diagram/" title="MJS Diagram">
+            <Button variant={"ghost"} className="rounded-full" size={"icon"}>
+              <Image
+                src={"./markerjs-logo-m.svg"}
+                alt="MJS Diagram"
+                width="24"
+                height="24"
+                className="h-4 w-4"
+              />
+            </Button>
+          </a>
+
+          <a href="https://github.com/ailon/diagram-app" title="MJS Diagram">
+            <Button variant={"ghost"} className="rounded-full" size={"icon"}>
+              <GitHubLogoIcon className="h-4 w-4 text-black" />
+            </Button>
+          </a>
           <Link href="/about">
             <Button variant={"ghost"} className="rounded-full" size={"icon"}>
               <QuestionMarkIcon />
